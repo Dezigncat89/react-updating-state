60df77694048831ad8230cc7d701d53cdca59b6a
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-18 16:08:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+class LightSwitch extends React.Component {
+  constructor() {
+    super();
+
+    // Initial state is defined
+    this.state = {
+      toggled: false
+    };
+  }
+
+  // when handleClick is called, setState will update the state so that toggle is reversed
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        toggled: !previousState.toggled
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON" : "OFF"}</button>
+      </div>
+    );
+  }
+}
+
+export default LightSwitch;
